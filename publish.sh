4d55215fea0e83d02e517f567a45db6458506c0d
javis-yan <yan_h@hotmail.com>
2018-04-16 22:12:37 +0800

    Update date format

--- publish.sh
+++ publish.sh
@@ -4,12 +4,12 @@ set -o nounset                              # Treat unset variables as an error
 
 rm -rf pages
 git clone -b gh-pages \
-    git@github.com:diwakergupta/thrift-missing-guide.git pages
+    git@github.com:nobodyhub-learn/thrift-missing-guide.git pages
 mv index.html thrift.pdf pages
 
 pushd pages
-git config user.name "Diwaker Gupta"
-git config user.email diwakergupta@gmail.com
+git config user.name "yan-hai"
+git config user.email yan_h@hotmail.com
 git add index.html thrift.pdf
 git commit -m Update
 git push origin HEAD:gh-pages
